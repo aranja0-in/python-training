9efad0b8c5940764ddfba861ad22e8653888a85e
Amit <aranja0.in@gmail.com>
2020-08-02 08:06:42 +0000

    More assignments

---
+++ basics/assignment.py
@@ -0,0 +1,19 @@
+# SyntaxError
+# new@ = 5 
+# print(new@)  
+
+# Invalid syntax
+# global = 70
+# print(global)
+
+# Implicit multi line
+# new = { 2:3,
+#         5:7,
+#         9:11
+#     }
+# print(new)
+
+x = 100
+print(x)
+del(x)
+print(x)
---
+++ flow-control/main.py
@@ -0,0 +1,16 @@
+# x = 20
+# y = 3
+# c = x//y 
+
+# if(c < 0):
+#     print('nmber is positive')
+# else: 
+#     print('number is negative')
+
+
+# for x in range(5):
+#     print(x)
+
+
+for x in range(4, 10, 2):
+    print(x)
