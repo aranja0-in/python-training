7e7ca573d8a4e71c2a9f09216f16de0fbcd9b89e
Amit <aranja0.in@gmail.com>
2020-08-03 07:59:34 +0000

    func added

---
+++ func/main.py
@@ -0,0 +1,53 @@
+# Pass by ref
+# def fun(x) :
+#     x[0] = 20
+#     return x
+
+# lst = [10, 11, 12, 13, 14, 15]
+# fun(lst)
+
+# print(lst)
+
+# Pass by value
+# def new(a) :
+#     x = '2' + a
+#     print(x)
+#     print(id(x))
+
+# a = "hello"
+# new(a)
+
+# print(a)
+# print(id(a))
+
+# Required Arguments
+
+# def sum_list(items, items1):
+#     sum_numbers = 0
+#     for x in items:
+#         sum_numbers += x
+#         return sum_numbers
+
+# lst = [1, 2, -8]
+# print(sum_list(lst))
+
+# Keyword Arguments
+
+# def printinfo(name, age):
+#     print("Name: ", name)
+#     print("Age: ", age)
+#     return
+
+# printinfo( age= 50, name = "miki")
+
+# Default Arguments
+
+# def printinfo(name, age = 35):
+#     print("Name: ", name)
+#     print("Age: ", age)
+#     return
+
+# printinfo( age= 50, name = "miki")
+# printinfo( name = "miki")
+
+# Variable Arguments
