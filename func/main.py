1c30e22ee50b634e42d3986a21bd2b8c1e6a9a41
Amit <aranja0.in@gmail.com>
2020-08-03 13:41:28 +0530

    commented code

--- func/main.py
+++ func/main.py
@@ -52,10 +52,10 @@
 
 # Variable Arguments
 
-def sum(*args):
-    total = 0
-    for i in args:
-        total += i
-    return total
+# def sum(*args):
+#     total = 0
+#     for i in args:
+#         total += i
+#     return total
 
-print(sum(1, 2, 3, 4))
+# print(sum(1, 2, 3, 4))
