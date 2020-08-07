bb6b81bf03103d40fcf20c64b8359e124eea03fa
Amit <aranja0.in@gmail.com>
2020-08-07 17:52:34 +0530

    added more samples

---
+++ class/main.py
@@ -0,0 +1,27 @@
+# class Employee():
+#     pass
+
+# emp1 = Employee()
+# emp1.name = "Sachin"
+# emp1.age = 45
+
+# emp2 = Employee()
+# emp2.name = "Dhoni"
+# emp2.age = 35
+
+# print(emp1.age)
+# print(emp2.name)
+
+# With Init function
+
+# class Employee():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def print(self):
+#         print(self.name, ":", self.age)
+
+
+# emp = Employee('Sachin', 24)
+# emp.print()
